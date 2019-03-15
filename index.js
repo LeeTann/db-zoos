24b292593e9db4ba4c126af13822dd2f5b165de2
ltann <leeroyleeroy18@gmail.com>
2019-03-14 19:01:08 -0700

    Created bears table and finished CRUD. Stretch completed

--- index.js
+++ index.js
@@ -80,6 +80,62 @@ server.delete('/api/zoos/:id', async (req, res) => {
   }
 })
 
+server.get('/api/bears', async (req, res) => {
+  try {
+    const bears = await db('bears')
+    res.status(200).json(bears)
+  } catch(error) {
+    res.status(500).json(error)
+  }
+})
+
+server.get('/api/bears/:id', async (req, res) => {
+  try {
+    const bears = await db('bears').where({ id: req.params.id }).first()
+    res.status(200).json(bears)
+  } catch(error) {
+    res.status(500).json(error)
+  }
+})
+
+server.post('/api/bears', async (req, res) => {
+  try{
+    const [id] = await db('bears').insert(req.body)
+    const newBear = await db('bears').where({ id }).first()
+    res.status(201).json(newBear)
+  } catch(error) {
+    res.status(500).json(error)
+  }
+})
+
+server.put('/api/bears/:id', async (req, res) => {
+  try{
+    updatedBear = await db('bears').where({ id: req.params.id }).update(req.body)
+
+    if (updatedBear > 0) {
+      const bear = await db('bears').where({ id: req.params.id}).first()
+      res.status(200).json(bear)
+    } else {
+      res.status(404).json({ message: "record not found"})
+    }
+  } catch(error) {
+    res.status(500).json(error)
+  }
+})
+
+server.delete('/api/bears/:id', async (req, res) => {
+  try {
+    deletedBear = await db('bears').where({ id: req.params.id }).delete()
+
+    if (deletedBear > 0) {
+      res.status(204).end()
+    } else {
+      res.status(404).json({ message: "record not found"})
+    }
+  } catch(error) {
+    res.status(500).json(error)
+  }
+})
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
